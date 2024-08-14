95ddde5e0a4666a8605306457c7bf88325f4f5e1
luisrivera <USTR012524@ugb.edu.sv>
2024-08-14 09:47:41 -0600

    Add files via upload
    secuencia de fibonacci

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int a = 0, b = 0, c = 1;
+
+
+            /*
+             a+b=c
+             0+1=1
+             1+0=1
+             1+1=2
+            2+1=3
+            3+2=5
+            ...
+            */
+            while (c < 100) {
+                Console.WriteLine("{0}+{1}={2}", a, b, c);
+                b = a;
+                a = c;
+                c = a + b;
+            }
+            Console.ReadLine();
+        }
+    }
+}
